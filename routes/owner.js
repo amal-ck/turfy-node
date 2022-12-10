678b285991e6af2549a3ed4936ac6766b85ec216
Amal C K <amalckishere@gmail.com>
2022-12-10 20:22:44 +0530

    turf add design completed, values pending inside

--- routes/owner.js
+++ routes/owner.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* owner controlls. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('owner/view-turf',{owner:true,title:'owner'})
 });
+router.get('/add-turf',(req,res)=>{
+  res.render('owner/add-turf',{owner:true,title:'add turf'})
+})
+
 
 module.exports = router;
