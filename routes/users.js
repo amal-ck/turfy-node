a38713e9ae58686b160ad0e2142b3c9ae92931b3
Amal C K <amalckishere@gmail.com>
2023-01-19 12:24:18 +0530

    turf booking design completed

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,11 @@ var userHelpers=require('../helpers/users-helpers')
 /* GET user page. */
 router.get('/', (req, res)=>{
   userHelpers.viewTurf().then((turf)=>{
-    res.render('users/home',{user:true,title:'Trfu-Home',turf})
-  })
-  
+    res.render('users/home',{user:true,title:'Turfy Home',turf})
+  })  
 });
+router.get('/book-turf/:id',(req,res)=>{
+  res.render('users/view-turf',{user:true,title:'Book turf'})
+})
 
 module.exports = router;
